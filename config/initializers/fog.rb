5588703caa7b356e8f4310cfead577530e5e2006
“plantoteachme” <amyplant@me.com>
2016-10-07 17:36:29 -0500

    Edit AWS environment references

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,13 +1,16 @@
 CarrierWave.configure do |config|
+
+  #Required for Heroku
+  config.root = Rails.root.join('tmp')
+  config.cache_dir = 'carrierwave'
+
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-    :region                 => ENV['AWS_REGION']
+    :aws_access_key_id      => ENV['S3_ACCESS_KEY'],
+    :aws_secret_access_key  => ENV['S3_SECRET_KEY'],
+    :region                 => 'us-west-2'
   }
-  #Required for Heroku
-  config.cache_dir = "#{Rails.root}/tmp/uploads"
-  
-  config.fog_directory  = ENV['AWS_BUCKET']
+
+  config.fog_directory  = ENV['S3_BUCKET']
   config.fog_public     = false
 end
